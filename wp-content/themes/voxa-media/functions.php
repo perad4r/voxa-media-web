2d3b28e0e91b88d49c9b7cf68849bf72fa063958
Capoo <64675226+perad4r@users.noreply.github.com>
2026-09-26 11:22:22 +0700

    Standardize VOXA Media favicon variants

--- wp-content/themes/voxa-media/functions.php
+++ wp-content/themes/voxa-media/functions.php
@@ -8,27 +8,94 @@ function voxa_media_setup() {
 	add_theme_support('post-thumbnails');
 	add_theme_support('responsive-embeds');
 	add_theme_support('html5', ['search-form', 'comment-form', 'comment-list', 'gallery', 'caption', 'style', 'script']);
-	add_theme_support('site-icon');
 	add_theme_support('bbpress');
 	add_theme_support('yoast-seo-breadcrumbs');
 }
 add_action('after_setup_theme', 'voxa_media_setup');
 
+function voxa_media_disable_core_site_icon() {
+	remove_action('wp_head', 'wp_site_icon', 99);
+}
+add_action('after_setup_theme', 'voxa_media_disable_core_site_icon', 20);
+
+function voxa_media_asset_url($filename) {
+	$asset_path = get_stylesheet_directory() . '/assets/' . $filename;
+	$asset_url = get_stylesheet_directory_uri() . '/assets/' . rawurlencode($filename);
+
+	if (file_exists($asset_path)) {
+		$asset_url = add_query_arg('ver', (string) filemtime($asset_path), $asset_url);
+	}
+
+	return $asset_url;
+}
+
 function voxa_media_favicon() {
-	if (function_exists('has_site_icon') && has_site_icon()) {
-		return;
+	$icon_sizes = [
+		'favicon-16x16.png' => '16x16',
+		'favicon-32x32.png' => '32x32',
+		'favicon-48x48.png' => '48x48',
+		'favicon.png' => '512x512',
+	];
+
+	foreach ($icon_sizes as $filename => $size) {
+		$icon_path = get_stylesheet_directory() . '/assets/' . $filename;
+		if (!file_exists($icon_path)) {
+			continue;
+		}
+
+		printf(
+			'<link rel="icon" type="image/png" sizes="%1$s" href="%2$s">',
+			esc_attr($size),
+			esc_url(voxa_media_asset_url($filename))
+		);
 	}
 
-	$icon_path = get_stylesheet_directory() . '/assets/favicon.png';
-	if (!file_exists($icon_path)) {
-		return;
+	$apple_icon_path = get_stylesheet_directory() . '/assets/apple-touch-icon.png';
+	if (file_exists($apple_icon_path)) {
+		printf(
+			'<link rel="apple-touch-icon" sizes="180x180" href="%s">',
+			esc_url(voxa_media_asset_url('apple-touch-icon.png'))
+		);
 	}
 
-	$icon_url = add_query_arg('ver', (string) filemtime($icon_path), get_stylesheet_directory_uri() . '/assets/favicon.png');
-	printf('<link rel="icon" href="%1$s" type="image/png">', esc_url($icon_url));
-	printf('<link rel="apple-touch-icon" href="%1$s">', esc_url($icon_url));
+	$manifest_path = get_stylesheet_directory() . '/assets/site.webmanifest';
+	if (file_exists($manifest_path)) {
+		$manifest_url = add_query_arg('ver', (string) filemtime($manifest_path), home_url('/site.webmanifest'));
+		printf('<link rel="manifest" href="%s">', esc_url($manifest_url));
+	}
 }
 add_action('wp_head', 'voxa_media_favicon', 98);
+
+function voxa_media_redirect_brand_assets() {
+	$request_uri = isset($_SERVER['REQUEST_URI']) ? wp_unslash($_SERVER['REQUEST_URI']) : '';
+	$request_path = wp_parse_url($request_uri, PHP_URL_PATH);
+	$brand_assets = [
+		'/favicon.ico' => 'favicon.png',
+		'/favicon.png' => 'favicon.png',
+		'/favicon-16x16.png' => 'favicon-16x16.png',
+		'/favicon-32x32.png' => 'favicon-32x32.png',
+		'/favicon-48x48.png' => 'favicon-48x48.png',
+		'/apple-touch-icon.png' => 'apple-touch-icon.png',
+		'/android-chrome-192x192.png' => 'android-chrome-192x192.png',
+		'/android-chrome-512x512.png' => 'android-chrome-512x512.png',
+		'/site.webmanifest' => 'site.webmanifest',
+	];
+
+	if (!isset($brand_assets[$request_path])) {
+		return;
+	}
+
+	$asset_filename = $brand_assets[$request_path];
+	$asset_path = get_stylesheet_directory() . '/assets/' . $asset_filename;
+	if (!file_exists($asset_path)) {
+		return;
+	}
+
+	nocache_headers();
+	wp_safe_redirect(voxa_media_asset_url($asset_filename), 302, 'VOXA Media');
+	exit;
+}
+add_action('template_redirect', 'voxa_media_redirect_brand_assets', 0);
 
 function voxa_media_enqueue_styles() {
 	$css_path = get_stylesheet_directory() . '/assets/css/site.css';
